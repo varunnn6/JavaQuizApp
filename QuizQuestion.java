5a7e529e621bad3d38d23902cbd87bf7bb3692cf
Varun Malik <malikvaron@gmail.com>
2026-01-16 03:25:15 +0530

    Update QuizQuestion.java

--- QuizQuestion.java
+++ QuizQuestion.java
@@ -3,6 +3,7 @@ public class QuizQuestion {
     private String question;
     private String[] options;
     private int correctAnswer;
+    private int selectedOption = -1; // store user-selected option
 
     public QuizQuestion(String question, String[] options, int correctAnswer) {
         this.question = question;
@@ -10,15 +11,12 @@ public class QuizQuestion {
         this.correctAnswer = correctAnswer;
     }
 
-    public String getQuestion() {
-        return question;
-    }
+    // Getters
+    public String getQuestion() { return question; }
+    public String[] getOptions() { return options; }
+    public int getCorrectAnswer() { return correctAnswer; }
 
-    public String[] getOptions() {
-        return options;
-    }
-
-    public int getCorrectAnswer() {
-        return correctAnswer;
-    }
+    // Selected answer methods
+    public void setSelectedOption(int index) { selectedOption = index; }
+    public int getSelectedOption() { return selectedOption; }
 }
